abc775e174b933f9ec74237a161b580f41b15c45
Xingyou <3217359147@qq.com>
2022-11-17 10:31:57 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/Gitjava.java
+++ src/main/java/com/atguigu/git/Gitjava.java
@@ -4,5 +4,6 @@ public class Gitjava {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("nihao");
+        System.out.println("hot-fix to fix");
     }
 }
