0309664d0b7759f4e717043d17b90806c76fdb76
Xingyou <3217359147@qq.com>
2022-11-17 10:00:18 +0800

    second commit

--- src/main/java/com/atguigu/git/Gitjava.java
+++ src/main/java/com/atguigu/git/Gitjava.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class Gitjava {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("nihao");
     }
 }
